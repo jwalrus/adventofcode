2fe9dfe770c9dad90d471a50be16c65a91f51ff8
jwalrus <jwalrus@protonmail.com>
2021-12-01 12:42:41 -0500

    feat: 2021 Day 01 (improved)

--- 2021/pyaoc/day01.py
+++ 2021/pyaoc/day01.py
@@ -1,23 +1,16 @@
-import itertools
-
-
 def load():
     with open("day01.txt") as f:
         xs = f.readlines()
         return [int(x) for x in xs]
 
 
-def part1(puzzle):
-    return sum([x > y for x, y in zip(puzzle[1:], puzzle[:-1])])
-
-
-def windows(xs, n=3):
-    for i, _ in enumerate(xs[:-n + 1]):
-        yield list(itertools.islice(xs, i, i + n))
+def part1(puzzle, n=1):
+    return sum([x > y for x, y in zip(puzzle[n:], puzzle[:-n])])
 
 
 def part2(puzzle):
-    return part1([sum(w) for w in windows(puzzle, n=3)])
+    # a + b + c < b + c + d => a < d
+    return part1(puzzle, n=3)
 
 
 class TestDay01:
@@ -27,10 +20,6 @@ class TestDay01:
 
     def test_part1(self):
         assert part1(load()) == 1482
-
-    def test_windows(self):
-        assert list(windows([1, 2, 3, 4], n=2)) == [[1, 2], [2, 3], [3, 4]]
-        assert list(windows([1, 2, 3, 4, 5], n=2)) == [[1, 2], [2, 3], [3, 4], [4, 5]]
 
     def test_part2_sample(self):
         assert part2([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5
